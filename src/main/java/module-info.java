4274eb74d6ffffbc74a069d42603b100f3407c23
Audran Bert <57795315+AudranBert@users.noreply.github.com>
2022-03-21 15:34:40 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.m1prototyopage {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.m1prototyopage to javafx.fxml;
+    exports com.example.m1prototyopage;
+}
