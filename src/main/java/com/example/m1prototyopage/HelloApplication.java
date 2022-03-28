c7307662a6bd02247f1dd552c596d1dd8ea33c8d
bensafisara <benfisara2@gmail.com>
2022-03-28 15:17:47 +0200

    pull database

--- src/main/java/com/example/m1prototyopage/HelloApplication.java
+++ src/main/java/com/example/m1prototyopage/HelloApplication.java
@@ -11,8 +11,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        scene.getStylesheets().add("Style.css");
+        stage.setTitle("Profil");
         stage.setScene(scene);
         stage.show();
     }
