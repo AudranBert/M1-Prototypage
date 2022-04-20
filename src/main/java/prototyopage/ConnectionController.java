1121ceac9b30ae98fd100d9f3a4bb88abc474939
= <=>
2022-04-20 15:39:29 +0200

    Lier les boutons au controller

--- src/main/java/prototyopage/ConnectionController.java
+++ src/main/java/prototyopage/ConnectionController.java
@@ -1,10 +1,22 @@
 package prototyopage;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ConnectionController {
     private MainApp mainApp;
+
+    @FXML
+    private Button connexionButton;
 
     public void setMainApp(MainApp mainApp)
     {
         this.mainApp = mainApp;
     }
+
+    @FXML
+    protected void connect() {
+        //to do
+    }
+
 }
